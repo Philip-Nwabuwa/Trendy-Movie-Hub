deda8c6b838ff54f21dfa0e0e1ffd3ecd27e4243
Philip Nwabuwa <philipnwabuwa@gmail.com>
2023-01-30 17:27:31 +0100

    Setting up app and installed tailwind

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import { Poppins } from "@next/font/google";
+
+const Poppins = Poppins({ subsets: ["poppins"] });
+
+export default function Home() {
+  return <main className="underline">app</main>;
+}
